6224aea47450e95e14c5b9ce96f9181cf765b7ec
Anthony Roberts <acrobert@gmail.com>
2022-01-10 12:33:27 -0800

    wordle: more convenient selector syntax

--- wordle.sh
+++ wordle.sh
@@ -14,6 +14,8 @@ function histogram () {
   sed "s,.,&\n,g" | grep -v ^$ | sort | uniq -c | sort -rn
 }
 
+SELECTOR="` echo "${4}" | sed "s,_\([a-z][a-z]*\)_,[^\1],g" `"
+
 if [ X$1 == "Xx" ]; then
   # exclude letters from candidate words
   grep $VALID_WORD_RE | ( exclude $2 )
@@ -24,7 +26,7 @@ elif [ X$1 == "Xc" ]; then
   # display letter histogram of candidate words
   grep $VALID_WORD_RE | ( histogram ) | ( exclude $2 )
 elif [ X$1 == "Xixc" ]; then
-  grep $VALID_WORD_RE | ( include $2 ) | ( exclude $3 ) | grep "^$4$" | ( histogram ) | ( exclude $2 )
+  grep $VALID_WORD_RE | ( include $2 ) | ( exclude $3 ) | grep "^${SELECTOR}$" | ( histogram ) | ( exclude $2 )
 elif [ X$1 == "Xix" ]; then
-  grep $VALID_WORD_RE | ( include $2 ) | ( exclude $3 ) | grep "^$4$"
+  grep $VALID_WORD_RE | ( include $2 ) | ( exclude $3 ) | grep "^${SELECTOR}$"
 fi
