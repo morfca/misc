b652e8f1be5c6fde0bf550cc89259b2ffbcda28d
Anthony Roberts <acrobert@gmail.com>
2022-01-09 22:37:07 -0800

    refactor with new combined modes

--- wordle.sh
+++ wordle.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+VALID_WORD_RE="^.....$"
+
+function exclude () {
+  grep -v `echo $1 | sed "s,.,-e & ,g"`
+}
+
+function include () {
+  awk "`echo $1 | sed "s,., /&/ \&\&,g" | sed "s, &&$,,"`"
+}
+
+function histogram () {
+  sed "s,.,&\n,g" | grep -v ^$ | sort | uniq -c | sort -rn
+}
+
 if [ X$1 == "Xx" ]; then
   # exclude letters from candidate words
-  grep -v `echo $2 | sed "s,.,-e & ,g"`
+  grep $VALID_WORD_RE | ( exclude $2 )
 elif [ X$1 == "Xi" ]; then
   # require candidate words include letters
-  awk "`echo $2 | sed "s,., /&/ \&\&,g" | sed "s, &&$,,"`"
+  grep $VALID_WORD_RE | ( include $2 )
 elif [ X$1 == "Xc" ]; then
   # display letter histogram of candidate words
-  sed "s,.,&\n,g" | grep -v ^$ | sort | uniq -c | sort -rn | grep -v `echo $2 | sed "s,.,-e & ,g"`
+  grep $VALID_WORD_RE | ( histogram ) | ( exclude $2 )
+elif [ X$1 == "Xixc" ]; then
+  grep $VALID_WORD_RE | ( include $2 ) | ( exclude $3 ) | grep "^$4$" | ( histogram ) | ( exclude $2 )
+elif [ X$1 == "Xix" ]; then
+  grep $VALID_WORD_RE | ( include $2 ) | ( exclude $3 ) | grep "^$4$"
 fi
